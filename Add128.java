6f40d8e2105e972497d358a95617a095942b1013
Greg <Greg@192.168.0.105>
2018-11-26 22:05:22 -0500

    Debugged and added necessary print statements

--- Add128.java
+++ Add128.java
@@ -21,23 +21,39 @@ public class Add128 implements SymCipher {
 
 	@Override
 	public byte[] encode(String S) {
+		System.out.print("Encrypting: String: " + S + " ");
 		byte[] message = S.getBytes();
+		System.out.print("Unencrypted bytes: ");
+		for(byte a: message)
+			System.out.print(a+" ");
 		int count = 0;
 		for(int i =0;i<message.length;i++) {
 			message[i]=(byte) (message[i]+key[count]);
-			count = (count+1)%count;
+			count = (count+1)%128;
 		}
+		System.out.print("Encrypted bytes: ");
+		for(byte a: message)
+			System.out.print(a+" ");
+		System.out.println("");
 		return message;
 	}
 
 	@Override
 	public String decode(byte[] bytes) {
+		System.out.print("Decrypting: Encrypted Bytes: ");
+		for(byte a: bytes)
+			System.out.print(a+" ");
 		int count = 0;
 		for(int i =0;i<bytes.length;i++) {
 			bytes[i]=(byte) (bytes[i]-key[count]);
-			count = (count+1)%count;
+			count = (count+1)%128;
 		}
-		return bytes.toString();
+		System.out.print("Decrypted Bytes: ");
+		for(byte a: bytes)
+			System.out.print(a+" ");
+		String s = new String(bytes);
+		System.out.println("String: " +s);
+		return s;
 	}
 
 }
--- Substitute.java
+++ Substitute.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Substitute implements SymCipher {
 	private byte[] key;
-	private byte[] decode;
+	private byte[] decode = new byte[256];
 	
 	public Substitute() {
 		ArrayList<Byte> temp = new ArrayList<Byte>();
@@ -10,9 +10,10 @@ public class Substitute implements SymCipher {
 			temp.add((byte)i);
 		key = new byte[256];
 		for(int i =0;i<256;i++) {
-			byte b = (byte)(Math.random()*(temp.size()-2));
+			byte b = temp.remove((int) (Math.random()*(temp.size()-2)));
 			key[i]=b;
-			decode[b]=(byte)i;
+			decode[b & 0xFF]=(byte)i;
+			//System.out.println((b&0xFF) +" " + decode[b & 0xFF] );
 		}
 			
 	}
@@ -20,8 +21,8 @@ public class Substitute implements SymCipher {
 	public Substitute(byte[] b) {
 		if(b.length!=256) return;
 		key= b;
-		for(int i=0;i<key.length;i++)
-			decode[i]=key[i];
+		for(int i=0;i<256;i++)
+			decode[key[i] & 0xFF]=(byte)i;
 	}
 	
 	@Override
@@ -31,17 +32,33 @@ public class Substitute implements SymCipher {
 
 	@Override
 	public byte[] encode(String S) {
+		System.out.print("Encrypting: String: "+S + " ");
 		byte[] message = S.getBytes();
+		System.out.print("Unencrypted bytes: ");
+		for(byte a: message)
+			System.out.print(a+" ");
 		for(int i =0;i<message.length;i++)
-			message[i]=key[i];
+			message[i]=key[message[i]];
+		System.out.print("Encrypted bytes: ");
+		for(byte a: message)
+			System.out.print(a+" ");
+		System.out.println("");
 		return message;
 	}
 
 	@Override
 	public String decode(byte[] bytes) {
+		System.out.print("Decrypting: Encrypted Bytes: ");
+		for(byte a: bytes)
+			System.out.print(a+" ");
 		for(int i=0;i<bytes.length;i++)
-			bytes[i]=decode[i];
-		return bytes.toString();
+			bytes[i]=decode[bytes[i] & 0xFF];
+		System.out.print("Decrypted Bytes: ");
+		for(byte a: bytes)
+			System.out.print(a+" ");
+		String s = new String(bytes);
+		System.out.println("String: " +s);
+		return s;
 	}
 
 }
